98bc1af5dd271fcdbd979a78575b6d3e39cdd02a
ceriosharon <ccerio73@gmail.com>
2024-10-06 07:26:31 +0300

    functions

---
+++ index.js
@@ -0,0 +1,19 @@
+// index.js
+
+function receivesAFunction(callback) {
+    callback();
+}
+
+function returnsANamedFunction() {
+    return function namedFunction() {
+        console.log("This is a named function.");
+    };
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {
+        console.log("This is an anonymous function.");
+    };
+}
+
+
